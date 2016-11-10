c3dfcd9b5429079400f2aca5c19f0a4b1ab35afd
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2016-11-09 21:22:17 -0500

    Lecture 75 - Understanding Navigation Paths

--- Exercise4/Routing-Start/src/app/user/user.component.ts
+++ Exercise4/Routing-Start/src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import { Component, OnDestroy } from '@angular/core';
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <a [routerLink]="['../']">home</a>
     `
 })
 export class UserComponent {
